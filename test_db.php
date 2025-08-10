5e17b7fc737d12ea38dbaeb2b821610bdcd4028c
Aakash sharma <Aakashsharmax01@gmail.com>
2025-08-11 00:35:28 +0530

    Delete test_db.php

--- test_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "portfolio_db");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-echo "✅ Connected successfully to MySQL!";
-?>
